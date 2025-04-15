37d509292cad5d10de55c1fe20fd004224a9e507
godfreyanabi <gongtupanabigodfrey@gmail.com>
2025-04-15 09:25:25 +0100

    message commit

---
+++ Documents/Object-Method/object.js
@@ -0,0 +1,10 @@
+// 15/April/2025
+
+// Object method
+
+let obj1 = {name:"Test"}
+let obj2 = {track:"web2"}
+let obj3 = {mode:"Game"}
+console.log("Before:",obj1)
+Object.assign(obj1, obj2, obj3)
+console.log("After:",obj1)

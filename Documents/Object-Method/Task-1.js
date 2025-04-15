de17ffff820262007348a74da6bc04c75a99ebbd
godfreyanabi <gongtupanabigodfrey@gmail.com>
2025-04-15 11:31:44 +0100

    message commit

---
+++ Documents/Object-Method/Task-1.js
@@ -0,0 +1,13 @@
+let personalInfor = {
+    name:"proph",
+    age:20,
+    gender:"M"
+};
+
+let userProfile = {
+    address:"Bukuru, Jos",
+    email: "gongtupanabigodfrey@gmail.com"
+    
+}
+Object.assign(personalInfor,userProfile)
+console.log(personalInfor,userProfile)
--- Documents/Object-Method/object.js
+++ Documents/Object-Method/object.js
@@ -2,9 +2,27 @@
 
 // Object method
 
-let obj1 = {name:"Test"}
-let obj2 = {track:"web2"}
-let obj3 = {mode:"Game"}
-console.log("Before:",obj1)
-Object.assign(obj1, obj2, obj3)
-console.log("After:",obj1)
+// let obj1 = {name:"Test"}
+// let obj2 = {track:"web2"}
+// let obj3 = {mode:"Game"}
+// console.log("Before:",obj1)
+// Object.assign(obj1, obj2, obj3)
+// console.log("After:",obj1)
+
+
+// Object creat
+
+// let obj1 = {name:"Test"}
+// let obj2 = {track:"web2"}
+// let obj3 = {mode:"Game"}
+// Object.assign(obj3, obj2, obj1)
+// let Obj4 = Objet.craet(obj2)
+// // console.log(obj4)
+// console.log("tract" in obj4)
+
+// let obj1 = {name:"Test", class:"web2", age: 30}
+// let keys = Object.keys(obj1)
+
+// if(keys.includes("name")){
+//     console.log(obj1.name)
+// }
